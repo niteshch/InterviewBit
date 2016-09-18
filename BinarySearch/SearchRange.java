1cebfdd5313a6ffcd3f363b461ef382e1175f9a3
niteshch <nc2663@columbia.edu>
2016-09-18 00:11:06 -0400

    Updating comments

--- BinarySearch/SearchRange.java
+++ BinarySearch/SearchRange.java
@@ -7,7 +7,7 @@ import java.util.List;
 Search for a Range
 Given a sorted array of integers, find the starting and ending position of a given target value.
 
-Your algorithm’s runtime complexity must be in the order of O(log n).
+Your algorithm's runtime complexity must be in the order of O(log n).
 
 If the target is not found in the array, return [-1, -1].
 
