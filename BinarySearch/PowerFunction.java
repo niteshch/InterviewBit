8c55298833004cc54c2b4eb820bcacab0384ef8a
niteshch <nc2663@columbia.edu>
2016-09-18 00:11:30 -0400

    Updating instructions

--- BinarySearch/PowerFunction.java
+++ BinarySearch/PowerFunction.java
@@ -1,44 +1,49 @@
 package BinarySearch;
+
 /*
-Implement Power Function
-Implement pow(x, n) % d.
+ Implement Power Function
+ Implement pow(x, n) % d.
 
-In other words, given x, n and d,
+ In other words, given x, n and d,
 
-find (xn % d)
+ find (x^n % d)
 
-Note that remainders on division cannot be negative. 
-In other words, make sure the answer you return is non negative.
+ Note that remainders on division cannot be negative. 
+ In other words, make sure the answer you return is non negative.
 
-Input : x = 2, n = 3, d = 3
-Output : 2
+ Input : x = 2, n = 3, d = 3
+ Output : 2
 
-2^3 % 3 = 8 % 3 = 2.
-See Expected Output
+ 2^3 % 3 = 8 % 3 = 2.
+ See Expected Output
 
  */
-public class PowerFunction{
-public int pow(int x, int n, int d) {
-	    
-	    long a = x;
-	    long res = 1L;
-	    
-	    while (n > 0) {
-	        
-	        if (n % 2 == 1) {
-	            res *= a;
-	            res %= d;
-	        }
-	        
-	        a *= a;
-	        a %= d;
-	        n = n >> 1;
-	        
-	    }
-	    
-	    res = (res + d) % d;
-	    
-	    return (int) res;
-	    
+public class PowerFunction {
+	public static void main(String[] args) {
+		new PowerFunction().pow(-2, 3, 3);
+	}
+	public int pow(int x, int n, int d) {
+
+		long a = x;
+		long res = 1L;
+
+		while (n > 0) {
+
+			if (n % 2 == 1) {
+				res *= a;
+				res %= d;
+				System.out.println(res);
+			}
+
+			a *= a;
+			a %= d;
+			n = n >> 1;
+
+		}
+
+		res = (res + d) % d;
+
+		return (int) res;
+
 	}
 }
